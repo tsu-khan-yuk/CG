bd069da3f8307609ec59ec0eb8f2dc1a237cf2e4
tsu-khan-yuk <62913085+tsu-khan-yuk@users.noreply.github.com>
2020-10-25 12:02:00 +0200

    added DDA and algo manager

--- lab_1/main.py
+++ lab_1/main.py
@@ -21,38 +21,56 @@ class Dot:
         return '({}, {})'.format(self.x, self.y)
 
 
-class Line:
-    start = None
-    end = None
-
-    def __init__(self, start: Dot, end: Dot) -> None:
-        if isinstance(start, Dot) and isinstance(end, Dot):
-            self.start = start
-            self.end = end
-        else:
-            raise TypeError('Invalid "start" or "end" type')
-
-    def __str__(self):
-        return '[{}, {}]'.format(str(self.start), str(self.end))
-
-
 class Algorithms:
-    base = {
-        'S': [
-            # (Dot(), Dot()), 
-            [Dot(20, 5), Dot(10, 10)], 
-            [Dot(10, 10), Dot(30, 30)],
-            # (Dot(), Dot()),
-            # (Dot(), Dot()) 
+    word = {
+        'S': [ 
+            [Dot(20, 6), Dot(30, 16)], 
+            [Dot(10, 16), Dot(20, 6)],
+            [Dot(10, 16), Dot(30, 36)],
+            [Dot(10, 36), Dot(20, 46)],
+            [Dot(20, 46), Dot(30, 36)]
         ],
-        'u': None,
-        'k': None,
-        'h': None,
-        'a': None,
-        'n': None,
-        'i': None,
-        'u': None,
-        'k': None
+        'u1': [
+            [Dot(35, 26), Dot(35, 46)],
+            [Dot(35, 46), Dot(45, 46)],
+            [Dot(45, 46), Dot(48, 44)],
+            [Dot(48, 26), Dot(48, 46)]
+        ],
+        'h':  [
+            [Dot(53, 6), Dot(53, 46)],
+            [Dot(53, 26), Dot(63, 26)],
+            [Dot(63, 26), Dot(66, 29)],
+            [Dot(66, 29), Dot(66, 46)]
+        ],
+        'a': [
+            [Dot(71, 26), Dot(71, 46)],
+            [Dot(71, 26), Dot(84, 26)],
+            [Dot(71, 46), Dot(81, 46)],
+            [Dot(81, 46), Dot(84, 43)],
+            [Dot(84, 26), Dot(84, 46)]
+        ],
+        'n': [
+            [Dot(89, 26), Dot(89, 46)],
+            [Dot(89, 29), Dot(97, 26)],
+            [Dot(97, 26), Dot(99, 26)],
+            [Dot(99, 26), Dot(102, 28)],
+            [Dot(102, 29), Dot(102, 46)]
+        ],
+        'i': [
+            [Dot(107, 26), Dot(107, 46)],
+            [Dot(107, 23), Dot(107, 22)],
+        ],
+        'u2': [
+            [Dot(112, 26), Dot(112, 46)],
+            [Dot(112, 46), Dot(122, 46)],
+            [Dot(122, 46), Dot(125, 44)],
+            [Dot(125, 26), Dot(125, 46)],
+        ],
+        'k': [
+            [Dot(130, 6), Dot(130, 46)],
+            [Dot(130, 36), Dot(140, 26)],
+            [Dot(130, 36), Dot(140, 46)],
+        ]
     }
 
     def __init__(self):
@@ -63,17 +81,17 @@ class Algorithms:
         frame = tk.Frame(root, bg='black')
         frame.pack()
 
-        dda = tk.Button(frame, text='\t\tDDA\t\t', command=self.command_manager('DDA'))
+        dda = tk.Button(frame, text='\t\tDDA\t\t', command=self.algorithm_manger('DDA'))
         dda.grid(row=1, column=1)
 
         root.mainloop()
 
-    def command_manager(self, cmd_name: str):
-        if cmd_name == 'DDA':
-            def func():
-                for i in self.base['S']:
-                    self.DDA(i[0], i[1])
-            return func
+    def algorithm_manger(self, algorithm_name: str):
+        def func():
+            for letter in self.word.keys():
+                for coords in self.word[letter]:
+                    getattr(self, algorithm_name)(coords[0], coords[1])
+        return func
 
     def draw(self, point: Dot):
         self.canvas.create_rectangle(
@@ -82,25 +100,23 @@ class Algorithms:
             PIXEL_SIZE * point.x + PIXEL_SIZE,
             PIXEL_SIZE * point.y + PIXEL_SIZE,
             fill="white",
-            tag="base"
+            tag="word"
         )
 
     def DDA(self, dot_1: Dot, dot_2: Dot) -> None:
 
-        if np.abs(dot_2.x - dot_1.x) >= np.abs(dot_2.y - dot_1.y):
-            length = np.abs(dot_2.x - dot_1.x)
-        else:
-            length = np.abs(dot_2.y - dot_1.y)
+        condition = np.abs(dot_2.x - dot_1.x) >= np.abs(dot_2.y - dot_1.y)
+        length = np.abs(dot_2.x - dot_1.x) if condition else np.abs(dot_2.y - dot_1.y)
 
         delta_x = (dot_2.x - dot_1.x) / length
         delta_y = (dot_2.y - dot_1.y) / length
 
-        x = dot_1.x + 0.5*np.sign(delta_x)
-        y = dot_2.y + 0.5*np.sign(delta_y)
+        x = dot_1.x
+        y = dot_1.y
 
         i = 0
         while i <= length:
-            self.draw(Dot(x, y))
+            self.draw(Dot(round(x), round(y)))
             x += delta_x
             y += delta_y
             i += 1
